478dee1da8fa1295dc08b194d5a2958858357e16
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-08-08 17:06:22 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,85 @@
+from logging import getLogger,config as logging_conf
+logger_config = {}
+logger_config['version'] = 1
+logger_config['disable_existing_loggers'] = False
+logger_config['formatters'] = {}
+logger_config['formatters']['simple'] = {}
+logger_config['formatters']['simple']['format'] = '%(asctime)s %(name)s:%(lineno)s %(funcName)s [%(levelname)s]: %(message)s'
+logger_config['formatters']['simple']['format'] = '%(asctime)s %(levelname)s: %(message)s'
+logger_config['handlers'] = {}
+logger_config['handlers']['consoleHandler'] = {}
+logger_config['handlers']['consoleHandler']['class'] = 'logging.StreamHandler'
+logger_config['handlers']['consoleHandler']['level'] = 'INFO'
+logger_config['handlers']['consoleHandler']['formatter'] = 'simple'
+logger_config['handlers']['consoleHandler']['stream'] = 'ext://sys.stdout'
+logger_config['handlers']['fileHandler'] = {}
+logger_config['handlers']['fileHandler']['class'] = 'logging.FileHandler'
+logger_config['handlers']['fileHandler']['level'] = 'DEBUG'
+logger_config['handlers']['fileHandler']['formatter'] = 'simple'
+logger_config['handlers']['fileHandler']['filename'] = '/log/custom/console.log'
+logger_config['handlers']['discord'] = {}
+logger_config['handlers']['discord']['class'] = 'logging.FileHandler'
+logger_config['handlers']['discord']['level'] = 'DEBUG'
+logger_config['handlers']['discord']['formatter'] = 'simple'
+logger_config['handlers']['discord']['filename'] = '/log/custom/console.log'
+logger_config['handlers']['discord.http'] = {}
+logger_config['handlers']['discord.http']['class'] = 'logging.FileHandler'
+logger_config['handlers']['discord.http']['level'] = 'DEBUG'
+logger_config['handlers']['discord.http']['formatter'] = 'simple'
+logger_config['handlers']['discord.http']['filename'] = '/log/custom/console.log'
+logger_config['loggers'] = {}
+logger_config['loggers']['__main__'] = {}
+logger_config['loggers']['__main__']['level'] = 'DEBUG'
+logger_config['loggers']['__main__']['handlers'] = ['consoleHandler', 'fileHandler']
+logger_config['loggers']['__main__']['propagate'] = False
+logger_config['loggers']['same_hierarchy'] = {}
+logger_config['loggers']['same_hierarchy']['level'] = 'DEBUG'
+logger_config['loggers']['same_hierarchy']['handlers'] = ['consoleHandler', 'fileHandler']
+logger_config['loggers']['same_hierarchy']['propagate'] = False
+logger_config['loggers']['lower.sub'] = {}
+logger_config['loggers']['lower.sub']['level'] = 'DEBUG'
+logger_config['loggers']['lower.sub']['handlers'] = ['consoleHandler', 'fileHandler']
+logger_config['loggers']['lower.sub']['propagate'] = False
+logger_config['root'] = {}
+logger_config['root']['level'] = 'INFO'
+logging_conf.dictConfig(logger_config)
+logger = getLogger(__name__)
+logger.info('Init')
+
+import os,sys
+import hashlib
+import traceback
+import discord
+import json
+import datetime
+import math
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN_DISCORD=os.environ['TOKEN_DISCORD']
+if len(TOKEN_DISCORD) > 0:
+    logger.info('Load & set the token DISCORD {0}..{1}'.format(
+        hashlib.sha1(TOKEN_DISCORD.encode()).hexdigest()[0:7],
+        hashlib.sha1(TOKEN_DISCORD.encode()).hexdigest()[-7:],
+    ))
+else:
+    raise ValueError('Require the token.discord')
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.reactions = True
+intents.typing = True
+client = discord.Client(intents=intents)
+
+# botを起動
+def main():
+    logger.info('Connecting to Discord API')
+    try:
+        client.run(TOKEN_DISCORD)
+    except discord.errors.PrivilegedIntentsRequired:
+        logger.error(traceback.format_exc())
+        sys.exit(1)
+
+logger.info(__name__)
+if __name__ == '__main__':
+    main()
